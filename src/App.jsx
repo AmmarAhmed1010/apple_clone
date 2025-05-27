3246bdadba25577421d05793fd14f9f5ae08e807
Ammar Ahmed <ammar7298@gmail.com>
2025-05-26 22:07:33 -0700

    spotlight added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Spotlight from './components/Spotlight'
 
 const App = () => {
   return (
   <main className='bg-black'>
    <Navbar/>
    <Hero/>
+   <Spotlight/>
   </main>
   )
 }
