39f06f1eeeb9ef220213a6823a126d467375504e
Ammar Ahmed <ammar7298@gmail.com>
2025-05-26 21:03:37 -0700

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Highlights from './components/Highlights'
+import Highlights from './components/HIghlights'
 
 const App = () => {
   return (
