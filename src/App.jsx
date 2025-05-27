69071b7214c766114d7a400b36bacbb64d69aea4
Ammar Ahmed <ammar7298@gmail.com>
2025-05-26 21:06:40 -0700

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import HIghlights from './components/HIghlights'
 
 const App = () => {
   return (
   <main className='bg-black'>
    <Navbar/>
    <Hero/>
-   <HIghlights/>
   </main>
   )
 }
--- src/components/Highlights.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HIghlights = () => {
-  return (
-    <div>HIghlights</div>
-  )
-}
-
-export default HIghlights
